1c0a9c4ab7d25a95805b824a3c839ce1311228c6
jakeimagelab <jakeimagelab@gmail.com>
2026-06-10 01:53:18 +0900

    Add files via upload

---
+++ app/hospital-instagram-manager/page.tsx
@@ -0,0 +1,11 @@
+import EmbedPage from "@/components/EmbedPage";
+
+export default function HospitalInstagramManagerPage() {
+  return (
+    <EmbedPage
+      title="병원 인스타그램 매니저"
+      badge="Instagram Manager"
+      src="/tools/hospital-instagram-manager/index.html"
+    />
+  );
+}
