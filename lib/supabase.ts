019ae6e5313ad22ba8e8ce54063259b2153bd3a8
jakeimagelab <jakeimagelab@gmail.com>
2026-06-10 18:52:00 +0900

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url  = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const url  = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://ncihdavftsjvyyuitjbp.supabase.co";
 const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const svc  = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
