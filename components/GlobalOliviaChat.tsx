7502bcb5cde363807d13d65af81b6dba0ac2a22d
jakeimagelab <jakeimagelab@gmail.com>
2026-06-14 14:46:10 +0900

    Add files via upload

---
+++ components/GlobalOliviaChat.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import OliviaChat from "@/components/OliviaChat";
+
+const localContextPages = ["/conti", "/photoclinic"];
+
+export default function GlobalOliviaChat() {
+  const pathname = usePathname();
+  const hasLocalOlivia = localContextPages.some((path) => pathname?.startsWith(path));
+
+  if (hasLocalOlivia) return null;
+
+  return <OliviaChat pageContext="월간 포토클리닉 구독 콘텐츠 운영 시스템" />;
+}
