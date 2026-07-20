a73d52ed8e890d82f04cf1d449309d25695d1c03
jakeimagelab <jakeimagelab@gmail.com>
2026-07-20 13:48:21 +0900

    Update OliviaChat.tsx

--- components/OliviaChat.tsx
+++ components/OliviaChat.tsx
@@ -221,7 +221,7 @@ const AUTO_EXECUTE_TOOLS = new Set([
   "get_workflow_status", "advance_workflow_step",
   "get_gallery", "create_gallery", "list_mailing_queue",
   "memo_add", "manage_olivia_action", "link_meeting_client",
-  "check_recent_errors", "generate_document",
+  "check_recent_errors", "generate_document", "generate_dev_request",
 ]);
 
 const CRUD_FIELD_LABELS: Record<string, string> = {
