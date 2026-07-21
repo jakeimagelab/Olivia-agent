7a048da57cbc3bb047f94d1cb01d598239c7ef23
jakeimagelab <jakeimagelab@gmail.com>
2026-07-21 22:34:43 +0900

    Update page.tsx

--- app/prompter/page.tsx
+++ app/prompter/page.tsx
@@ -683,11 +683,3 @@ export default function PrompterPage() {
     </div>
   );
 }
-
-function ChevronRightIcon() {
-  return (
-    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-      <path d="m9 18 6-6-6-6" />
-    </svg>
-  );
-}
