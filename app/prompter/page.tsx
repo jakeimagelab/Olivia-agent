395d996d542a17c7fa01ce2e983053134f53a2c5
jakeimagelab <jakeimagelab@gmail.com>
2026-07-22 21:54:11 +0900

    Update page.tsx

--- app/prompter/page.tsx
+++ app/prompter/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-
 import { useEffect, useRef, useState, useCallback } from "react";
 import {
   FlipHorizontal, FlipVertical, Play, Pause, RotateCcw,
