b5e2f9381eaf79068cbed2087f24747a6f76edb2
jakeimagelab <jakeimagelab@gmail.com>
2026-07-27 19:11:17 +0900

    Update PcrmSubNav.tsx

--- components/pcrm/PcrmSubNav.tsx
+++ components/pcrm/PcrmSubNav.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import {
   Building2,
   Calendar,
