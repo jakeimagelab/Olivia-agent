ad3d605694efa37906adbbe0be16787ca12b0779
jakeimagelab <jakeimagelab@gmail.com>
2026-08-05 23:05:35 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -248,6 +248,7 @@ export const config = {
     "/website-builder", "/website-builder/:path*",
     "/seo-delivery", "/seo-delivery/:path*",
     "/photo-sorting", "/photo-sorting/:path*",
+    "/broll-prompt", "/broll-prompt/:path*",
     "/prompter", "/prompter/:path*",
     "/video-sorting", "/video-sorting/:path*",
     "/video-convert", "/video-convert/:path*",
