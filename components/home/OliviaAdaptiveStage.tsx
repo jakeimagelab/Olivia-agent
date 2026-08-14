737091076371d9f8011851731886f7276edd6fbd
jakeimagelab <jakeimagelab@gmail.com>
2026-08-14 17:55:52 +0900

    Update OliviaAdaptiveStage.tsx

--- components/home/OliviaAdaptiveStage.tsx
+++ components/home/OliviaAdaptiveStage.tsx
@@ -20,6 +20,7 @@ export default function OliviaAdaptiveStage() {
   const workspaceFocused = useOliviaLayoutStore((state) => state.workspaceFocused);
   const setWorkspaceFocused = useOliviaLayoutStore((state) => state.setWorkspaceFocused);
   const openWorkspaceMode = useOliviaLayoutStore((state) => state.openWorkspaceMode);
+  const resetToIdle = useOliviaLayoutStore((state) => state.resetToIdle);
   const isStreaming = useOliviaConversationStore((state) => state.isStreaming);
   const pendingWorkspaceOpen = useOliviaConversationStore((state) => state.pendingWorkspaceOpen);
   const agentStatus = useOliviaConversationStore((state) => state.agentStatus);
