39afba4066fe0e8a0912b54099e771ce902ab34c
jakeimagelab <jakeimagelab@gmail.com>
2026-08-17 02:05:52 +0900

    Update QuoteBuilder.tsx

--- components/quote/QuoteBuilder.tsx
+++ components/quote/QuoteBuilder.tsx
@@ -4,7 +4,6 @@ import type { ReactElement, ReactNode } from "react";
 import { forwardRef, useEffect, useImperativeHandle, useMemo, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import OliviaChat from "@/components/OliviaChat";
-import { useOliviaChatModeStore } from "@/lib/store/useOliviaChatModeStore";
 import PageHeader from "@/components/PageHeader";
 import { createMailingDraft } from "@/lib/mailingQueue";
 import { useSaveShortcut } from "@/lib/hooks/useSaveShortcut";
