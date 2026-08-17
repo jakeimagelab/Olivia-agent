e1145c9fa27e2377cae3ef327a1136d8ee181f9d
jakeimagelab <jakeimagelab@gmail.com>
2026-08-18 03:13:29 +0900

    Update ContiBuilder.tsx

--- components/conti/ContiBuilder.tsx
+++ components/conti/ContiBuilder.tsx
@@ -7,6 +7,7 @@ import OliviaChat from "@/components/OliviaChat";
 import { uploadWorkflowArtifact } from "@/lib/workflowArtifacts";
 import { createMailingDraft } from "@/lib/mailingQueue";
 import { C } from "@/lib/theme";
+import ActiveMissionBar from "@/components/dashboard/ActiveMissionBar";
 import { useOliviaContextStore } from "@/lib/store/oliviaContextStore";
 import { addContiShots as addContiShotsShared, duplicateContiShot as duplicateContiShotShared, removeContiShot as removeContiShotShared, reorderContiShot as reorderContiShotShared, updateContiShot as updateContiShotShared } from "@/lib/conti/contiMutationService";
 import DrawingCanvas, { DrawingCanvasHandle, PEN_TYPES, DRAW_COLORS, ERASER_SIZES } from "@/components/DrawingCanvas";
