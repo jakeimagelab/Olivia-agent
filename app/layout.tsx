5c1fa3e7aff4fe0537d5cedc2bc4384a9fe8a29c
jakeimagelab <jakeimagelab@gmail.com>
2026-09-03 22:38:57 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import localFont from "next/font/local";
 import OliviaWorkspaceShell from "@/components/olivia/OliviaWorkspaceShell";
+import BackgroundJobsWidget from "@/components/olivia/BackgroundJobsWidget";
 import OliviaPageTransition from "@/components/olivia/OliviaPageTransition";
 import GlobalClientContextBridge from "@/components/GlobalClientContextBridge";
 import GlobalFeatureSidebar from "@/components/GlobalFeatureSidebar";
