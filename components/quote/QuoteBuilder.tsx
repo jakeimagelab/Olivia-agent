46c6f284d7d1ea278521425e56f205aff73dcb92
jakeimagelab <jakeimagelab@gmail.com>
2026-09-05 14:36:28 +0900

    Update QuoteBuilder.tsx

--- components/quote/QuoteBuilder.tsx
+++ components/quote/QuoteBuilder.tsx
@@ -4,6 +4,7 @@ import type { ReactElement, ReactNode } from "react";
 import { forwardRef, useEffect, useImperativeHandle, useLayoutEffect, useMemo, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import GlobalHeader from "@/components/GlobalHeader";
+import WorkspaceActionBar from "@/components/WorkspaceActionBar";
 import ActiveMissionBar from "@/components/dashboard/ActiveMissionBar";
 import { createMailingDraft } from "@/lib/mailingQueue";
 import { useSaveShortcut } from "@/lib/hooks/useSaveShortcut";
