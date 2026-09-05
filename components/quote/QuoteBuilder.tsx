bb1850691601a4410d095e55c3563c606e2e9c49
jakeimagelab <jakeimagelab@gmail.com>
2026-09-05 15:12:10 +0900

    Update QuoteBuilder.tsx

--- components/quote/QuoteBuilder.tsx
+++ components/quote/QuoteBuilder.tsx
@@ -10,6 +10,7 @@ import { createMailingDraft } from "@/lib/mailingQueue";
 import { useSaveShortcut } from "@/lib/hooks/useSaveShortcut";
 import { uploadWorkflowArtifact } from "@/lib/workflowArtifacts";
 import { useOliviaContextStore } from "@/lib/store/oliviaContextStore";
+import { useDesktopWindowMode } from "@/lib/desktopWindowContext";
 import { useQuoteStore } from "@/lib/store/useQuoteStore";
 import type { Brand, BenefitItem, CustomItem, CustomerInfo } from "@/lib/quote/quoteFormTypes";
 import { packages, singleItems, BRAND_CONFIG, type SingleItem } from "@/lib/quote/quoteCatalog";
