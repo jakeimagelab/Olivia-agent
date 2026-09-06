3ea10a6b275502d9feef71c5d740b71099dd60f5
jakeimagelab <jakeimagelab@gmail.com>
2026-09-06 09:30:43 +0900

    Update page.tsx

--- app/mailing/page.tsx
+++ app/mailing/page.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 import GlobalHeader from "@/components/GlobalHeader";
+import SegmentedTabs from "@/components/ui/SegmentedTabs";
 import { useSaveShortcut } from "@/lib/hooks/useSaveShortcut";
 import { useContactDirectory } from "@/lib/hooks/useContactDirectory";
 import { C } from "@/lib/theme";
