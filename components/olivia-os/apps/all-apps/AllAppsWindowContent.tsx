f0b5c00379d3210db4c7f403e1c93c96803da6a3
jakeimagelab <jakeimagelab@gmail.com>
2026-09-06 21:37:18 +0900

    Update AllAppsWindowContent.tsx

--- components/olivia-os/apps/all-apps/AllAppsWindowContent.tsx
+++ components/olivia-os/apps/all-apps/AllAppsWindowContent.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { ALL_TOOLS, groupToolsByCategory } from "@/lib/toolNav";
-import { Icon, type IconName } from "@/components/Icon";
+import type { IconName } from "@/components/Icon";
+import { AppIcon as ColorAppIcon } from "@/components/AppIcon";
 import { useOliviaDesktopStore } from "@/lib/store/useOliviaDesktopStore";
 import { useDesktopAppLauncher } from "../../useDesktopAppLauncher";
 import { AppIcon } from "../../AppIcon";
