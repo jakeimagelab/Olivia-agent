8c4ff096733939b7ed299e19e70c3dfd6b328522
jakeimagelab <jakeimagelab@gmail.com>
2026-09-09 02:19:15 +0900

    Update MemoWorkspace.tsx

--- components/memo/MemoWorkspace.tsx
+++ components/memo/MemoWorkspace.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { Plus, Trash2 } from "lucide-react";
 import GlobalHeader from "@/components/GlobalHeader";
