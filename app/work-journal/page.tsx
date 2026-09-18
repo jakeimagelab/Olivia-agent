f600404ab5f0bc9a0065ecf2d75e2e02f0ad6fcd
jakeimagelab <jakeimagelab@gmail.com>
2026-09-18 11:24:19 +0900

    Update page.tsx

--- app/work-journal/page.tsx
+++ app/work-journal/page.tsx
@@ -3,6 +3,7 @@
 import { Suspense, useCallback, useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import GlobalHeader from "@/components/GlobalHeader";
+import GeneralTasksPanel from "@/components/work-journal/GeneralTasksPanel";
 import ScheduleColumn from "@/components/work-journal/ScheduleColumn";
 import TodoColumn from "@/components/work-journal/TodoColumn";
 import PreparationColumn from "@/components/work-journal/PreparationColumn";
