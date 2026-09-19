b908288fa719fd0878d00b7049157cba4a2529e4
jakeimagelab <jakeimagelab@gmail.com>
2026-09-19 13:51:19 +0900

    Update OliviaDesktop.tsx

--- components/olivia-os/OliviaDesktop.tsx
+++ components/olivia-os/OliviaDesktop.tsx
@@ -9,7 +9,6 @@ import { DesktopSurface } from "./DesktopSurface";
 import { DesktopDock } from "./DesktopDock";
 import { DesktopGlobalSearch } from "./DesktopGlobalSearch";
 import { DesktopSystemOverlay, type DesktopOverlayKind, type WallpaperMode } from "./DesktopSystemOverlay";
-import { BackupReadyNotifications } from "./BackupReadyNotifications";
 import { oliviaAppRegistry } from "./registry/oliviaAppRegistry";
 import styles from "./OliviaDesktop.module.css";
 
