e7bb0834aad17363d6cbd5f64d5613f9e5518605
jakeimagelab <jakeimagelab@gmail.com>
2026-09-25 21:29:40 +0900

    Update shootingProgress.ts

--- lib/photo-storage/shootingProgress.ts
+++ lib/photo-storage/shootingProgress.ts
@@ -7,6 +7,7 @@ import {
   getWorkflowRun,
   revertWorkflowToStep,
 } from "@/lib/workflowAutomation";
+import { createEventDeduplicationKey, emitOliviaEvent } from "@/lib/olivia/events";
 import { validatePhotoProjectRelativePath } from "./server";
 
 export type ShootingProgressStage =
